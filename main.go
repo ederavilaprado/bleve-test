6200ff0c6b777ce278a0c7411f58d49cd9d13860
Eder Ávila Prado <prado.eder@gmail.com>
2017-10-03 15:59:16 -0300

    wip

--- main.go
+++ main.go
@@ -2,70 +2,76 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/blevesearch/bleve"
 )
+
+const datastorePath = "example.bleve"
+
+// NOTE: keyword é o analyser pra marca (não quebrar o token em palavras)
+// 0,5,10,15,20,25,30,35,40,45,50,55 * * * *
+// https://godoc.org/gopkg.in/robfig/cron.v2
 
 type product struct {
 	ID        int
 	Name      string
 	BrandName string
-	// Brand     brand
 }
 
-// type brand struct {
-// 	BrandName string
-// }
-
 func main() {
-	// index := bleve.NewDocumentMapping()
-	// brandMapping := bleve.NewTextFieldMapping()
-	// brandMapping.Store
 
-	// mapping := bleve.NewIndexMapping()
-	// index, err := bleve.New("example.bleve", mapping)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	// return
-	// }
-	// p1 := &product{1, "eder", "teste"}
-	// p2 := &product{2, "maria", "teste dois"}
-	// p3 := &product{3, "josé", "teste"}
-	// index.Index(strconv.Itoa(p1.ID), p1)
-	// index.Index(strconv.Itoa(p2.ID), p2)
-	// index.Index(strconv.Itoa(p3.ID), p3)
-
-	// NOTE: keyword é o analyser pra marca (não quebrar o token em palavras)
-
-	// 0,5,10,15,20,25,30,35,40,45,50,55 * * * *
-	// https://godoc.org/gopkg.in/robfig/cron.v2
-
-	index, err := bleve.Open("example.bleve")
+	index, err := openIndex()
 	if err != nil {
-		fmt.Println(err)
+		if err == bleve.ErrorIndexPathDoesNotExist {
+			in, err := createIndex()
+			if err != nil {
+				panic(err)
+			}
+			index = in
+		}
 	}
-	defer index.Close()
 
-	// d, err := index.Document("2")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Printf("=> %s\n", d.Fields[1].Value())
-
-	// search for some text
 	query := bleve.NewMatchQuery("teste")
+	search := bleve.NewSearchRequest(query)
 
 	stylesFacet := bleve.NewFacetRequest("BrandName", 5)
-	search := bleve.NewSearchRequest(query)
 	search.AddFacet("Brands", stylesFacet)
-	search.Fields = []string{"Name"}
+	search.Fields = []string{"Name", "BrandName"}
+
 	searchResults, err := index.Search(search)
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	fmt.Printf("=> %+v\n", searchResults)
+	fmt.Println(searchResults)
 
-	// fmt.Println(searchResults)
+}
 
+func openIndex() (bleve.Index, error) {
+	index, err := bleve.Open(datastorePath)
+	if err != nil {
+		return nil, err
+	}
+	return index, nil
+}
+
+func createIndex() (bleve.Index, error) {
+	// index := bleve.NewDocumentMapping()
+	// brandMapping := bleve.NewTextFieldMapping()
+
+	mapping := bleve.NewIndexMapping()
+	index, err := bleve.New(datastorePath, mapping)
+	if err != nil {
+		return nil, err
+	}
+	p1 := &product{1, "eder", "teste"}
+	p2 := &product{2, "maria", "teste dois"}
+	p3 := &product{3, "josé", "teste"}
+	index.Index(strconv.Itoa(p1.ID), p1)
+	index.Index(strconv.Itoa(p2.ID), p2)
+	index.Index(strconv.Itoa(p3.ID), p3)
+
+	return index, nil
 }
